4ed1b0636cd7704468fa7671cbefc98af0b15752
mackenziebishop <69914681+mackenziebishop@users.noreply.github.com>
2022-04-19 18:03:55 -0500

    Added updated packages

--- bishop/src/main/java/edu/nwmissouri/springbeam/bishop/MinimalPageRankBishop.java
+++ bishop/src/main/java/edu/nwmissouri/springbeam/bishop/MinimalPageRankBishop.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.beam.examples;
+package edu.nwmissouri.springbeam.bishop;
 
 // beam-playground:
 //   name: MinimalWordCount
