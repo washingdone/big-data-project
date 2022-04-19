146a439786579b0b87926248fccb5a1a011a260d
mackenziebishop <69914681+mackenziebishop@users.noreply.github.com>
2022-04-19 18:02:45 -0500

    Added RankedPage and VotingPage with new package

---
+++ bishop/src/main/java/edu/nwmissouri/springbeam/bishop/RankedPage.java
@@ -0,0 +1,10 @@
+package edu.nwmissouri.springbeam.bishop;
+
+import java.util.ArrayList;
+
+public class RankedPage {
+
+    public RankedPage(String key, ArrayList<VotingPage> voters) {
+    }
+    
+}
---
+++ bishop/src/main/java/edu/nwmissouri/springbeam/bishop/VotingPage.java
@@ -0,0 +1,8 @@
+package edu.nwmissouri.springbeam.bishop;
+
+public class VotingPage {
+
+    public VotingPage(String voterName, Integer contributorVotes) {
+    }
+    
+}
